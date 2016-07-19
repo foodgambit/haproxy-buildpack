0a2d47a62fc993ff818d8c0df269637e190248b6
foodgambit <foodgambit@users.noreply.github.com>
2016-07-19 10:00:30 -0500

    Remove port extraction as it should be in the environment already.

--- files/start_haproxy.sh
+++ files/start_haproxy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "About to configure haproxy"
-PORT=$(cat logs/env.log | grep -m 1 VCAP_APP_PORT | cut -d '=' -f 2)
 sed -i "s/\TEMP/$PORT/g" haproxy.cfg
 echo "haproxy configure:"
 cat haproxy.cfg
